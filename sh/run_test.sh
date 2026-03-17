7581bc6319bdb2cb3e09b5d69ff928cb3f2e01df
danila nikitin <danila.n2015@yandex.ru>
2026-03-17 14:58:28 +0300

    add .dockerignore, update .gitignore, and modify run_test.sh for coverage reports

--- sh/run_test.sh
+++ sh/run_test.sh
@@ -1,2 +1,6 @@
-ENV=test ./sh/migrate.sh
-pytest -v
+./sh/migrate.sh
+pytest -v ./test --cov=app \
+                 --cov=settings \
+                  --cov=db \
+                 --cov-report=xml:/app/reports/xml \
+                 --cov-report=html:/app/reports/html
